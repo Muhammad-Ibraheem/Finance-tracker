f1b10120a7973a15716041b0a3f89e6cabf2944a
Muhammad ibraheem <ibrahinaziz719@gmail.com>
2022-03-14 01:43:27 +0500

    create friendship model

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_312_193_255) do
+ActiveRecord::Schema.define(version: 20_220_313_195_714) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'friendships', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'friend_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['friend_id'], name: 'index_friendships_on_friend_id'
+    t.index ['user_id'], name: 'index_friendships_on_user_id'
+  end
 
   create_table 'stocks', force: :cascade do |t|
     t.string 'ticker'
@@ -41,10 +50,14 @@ ActiveRecord::Schema.define(version: 20_220_312_193_255) do
     t.datetime 'remember_created_at'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.string 'first_name'
+    t.string 'last_name'
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
+  add_foreign_key 'friendships', 'users'
+  add_foreign_key 'friendships', 'users', column: 'friend_id'
   add_foreign_key 'user_stocks', 'stocks'
   add_foreign_key 'user_stocks', 'users'
 end
